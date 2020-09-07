8c0657235368046269457e9705358db1d50ea6bd
biswatex <biswadeepam@gmail.com>
2020-09-07 18:21:40 +0530

    String Request added

--- app/src/main/java/com/biswatex/school/ExamsList.java
+++ app/src/main/java/com/biswatex/school/ExamsList.java
@@ -9,20 +9,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ExamsList extends AppCompatActivity {
 
+    String cls = "2", sec = "A";
     RecyclerView recyclerView;
     RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager layoutManager;
@@ -30,9 +35,11 @@ public class ExamsList extends AppCompatActivity {
     List<ExamModel> examModelList;
     String request_url = "http://biswatex.com/api/examsfetch.php";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_examinations);
         rq = Volley.newRequestQueue(this);
         recyclerView = findViewById(R.id.recyclerviewexaminations);
@@ -43,45 +50,66 @@ public class ExamsList extends AppCompatActivity {
         sendRequest();
     }
     public void sendRequest(){
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, request_url, null, new Response.Listener<JSONArray>() {
+//        Map<String, String> params = new HashMap<String, String>();
+//        params.put("cls", cls);
+//        params.put("sec", sec.toUpperCase());
+//
+//        JSONObject parameters = new JSONObject(params);
+        StringRequest jsonArrayRequest = new StringRequest(Request.Method.POST, request_url, new Response.Listener<String>() {
             @Override
-            public void onResponse(JSONArray response){
-                for(int i = 0; i < response.length(); i++){
-                    ExamModel examModel = new ExamModel();
-                    try{
-                        JSONObject jsonObject = response.getJSONObject(i);
-                        examModel.setExamid(jsonObject.getString("question_set_id"));
-                        examModel.setExamName(jsonObject.getString("teacher_id"));
-                        examModel.setExamClass(jsonObject.getString("class"));
-                        examModel.setexamDuration(jsonObject.getString("start_time"));
-                        examModel.setexamFullMarks(jsonObject.getString("full_marks"));
-                        examModel.setexamPassMarks(jsonObject.getString("pass_marks"));
-                        examModel.setexamSection(jsonObject.getString("sec"));
-                        examModel.setexamSubject(jsonObject.getString("sub"));
-                        examModel.setexamType(jsonObject.getString("type"));
-                        examModel.setexamResultOut(jsonObject.getString("end_time"));
-                        examModel.setExamTeacher(jsonObject.getString("teacher_name"));
-                    }catch(JSONException e){
-                        e.printStackTrace();
+            public void onResponse(String response) {
+                Log.i("Response",response);
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        ExamModel examModel = new ExamModel();
+                        try{
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            examModel.setExamid(jsonObject.getString("question_set_id"));
+                            examModel.setExamName(jsonObject.getString("teacher_id"));
+                            examModel.setExamClass(jsonObject.getString("class"));
+                            examModel.setexamDuration(jsonObject.getString("start_time"));
+                            examModel.setexamFullMarks(jsonObject.getString("full_marks"));
+                            examModel.setexamPassMarks(jsonObject.getString("pass_marks"));
+                            examModel.setexamSection(jsonObject.getString("sec"));
+                            examModel.setexamSubject(jsonObject.getString("sub"));
+                            examModel.setexamType(jsonObject.getString("type"));
+                            examModel.setexamResultOut(jsonObject.getString("end_time"));
+                            examModel.setExamTeacher(jsonObject.getString("teacher_name"));
+                        }catch(JSONException e){
+                            e.printStackTrace();
+                        }
+                        examModelList.add(examModel);
                     }
-                    examModelList.add(examModel);
+                    mAdapter = new CustomRecycleAdapter(ExamsList.this, examModelList,new  CustomRecycleAdapter.TakeExamListner(){
+                        @Override
+                        public void TakeExamOnClick(View v, int position) {
+                            Intent intent = new Intent(ExamsList.this, ActivityViewExam.class);
+                            intent.putExtra("exam_name", examModelList.get(position).getExamId());
+                            startActivity(intent);
+                        }
+                    });
+                    recyclerView.setAdapter(mAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                mAdapter = new CustomRecycleAdapter(ExamsList.this, examModelList,new  CustomRecycleAdapter.TakeExamListner(){
-                    @Override
-                    public void TakeExamOnClick(View v, int position) {
-                        Intent intent = new Intent(ExamsList.this, ActivityViewExam.class);
-                        intent.putExtra("exam_name", examModelList.get(position).getExamId());
-                        startActivity(intent);
-                    }
-                });
-                recyclerView.setAdapter(mAdapter);
+
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.i("Volley Error: ", error.toString());
             }
-        });
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("sec", sec);
+                params.put("cls", cls);
+                return params;
+            }
+        };
         rq.add(jsonArrayRequest);
     }
 }
